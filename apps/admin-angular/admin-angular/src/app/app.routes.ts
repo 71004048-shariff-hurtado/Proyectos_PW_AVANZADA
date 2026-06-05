8e1231c35217ba345d0f92d9240a4a64fa1647a1
Shariff Hurtado <shariffshs@gmail.com>
2026-06-05 10:41:09 -0500

    Agregando Componentes y Services

--- apps/admin-angular/admin-angular/src/app/app.routes.ts
+++ apps/admin-angular/admin-angular/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { ListaProductos } from './components/lista-productos/lista-productos';
 import { Validar } from './components/validar/validar';
 import { authGuardGuard } from './guard/auth-guard-guard';
 import { Usuario } from './components/usuario/usuario';
+import { Cursos } from './components/cursos/cursos';
+
 
 export const routes: Routes = [
     {path: 'producto', component: ListaProductos},
-    {path: 'validar', component: Validar, canActivate: [authGuardGuard]},
+    {path: 'cursos', component: Cursos},
     {path: 'usuario', component: Usuario},
     {path: '', redirectTo: 'validar' , pathMatch: 'full'}
 
